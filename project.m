15becc0174d2a528bc67078c10330bdfd4145fcd
Spark-Xl <sparkshenxl@gmail.com>
2016-11-30 23:48:53 +0800

    change return type of loaddata to three dimension array, run testing for every combination of data sets and save output

--- project.m
+++ project.m
@@ -1,28 +1,45 @@
 clear; close all;
-[data1, data2, data3, data4, data5, data6, data7, data8] = loadData();
+outputdata = loadData();
 right = 0;
 total = 0;
 options.MaxIter = 10;
 
-[traindata,testdata] = randomSplit(data1,0.9);
+[traindata,testdata] = randomSplit(outputdata(:,:,1),0.9);
 
 traindata = traindata;
 testdata = testdata;
 
-shape = size(data2);
+shape = size(outputdata(:,:,2));
 numofcol = shape(2);
 numofrow = shape(1);
+fileID = fopen('exp.txt','w');
+fprintf(fileID,datestr(now, 'dd-mm-yyyy HH:MM:SS'));
+fprintf(fileID,'\n');
 
-data = [data1;data2;data3];
-svmmodel = svmtrain(data(:,1:numofcol-2),data(:,numofcol));
-
-predlabel = [];
-for i = 1:1:numofrow
-    total = total + 1;%counter
-    predlabel = svmclassify(svmmodel,data7(i,1:numofcol-2));
-    if(data7(i,numofcol)==predlabel) %if the same
-        right = right + 1;%counter
+for q = 1:1:6
+    for r = q:1:6
+        for s = r:1:6
+            data = [outputdata(:,:,q);outputdata(:,:,r);outputdata(:,:,s)];
+            svmmodel = svmtrain(data(:,1:numofcol-2),data(:,numofcol));
+            
+            total = 0;
+            right = 0;
+            
+            predlabel = [];
+            for i = 1:1:numofrow
+                total = total + 1;%counter
+                data7 = outputdata(:,:,7);
+                predlabel = svmclassify(svmmodel,data7(i,1:numofcol-2));
+                if(data7(i,numofcol)==predlabel) %if the same
+                    right = right + 1;%counter
+                end
+            end
+            total
+            correct_rate = right/total
+            fprintf(fileID,'data_used: [%d, %d, %d]  #  ',q, r, s);
+            fprintf(fileID,'total: %d  #  ',total);
+            fprintf(fileID,'correct_rate: %f\n',correct_rate);
+        end
     end
 end
-total
-correct_rate = right/total
+fclose(fileID);
