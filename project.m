2e2e5ccfb1d00d40a5814bbd775c0392c8bd1a0a
Beibei Zhang <beibei.post@gmail.com>
2016-11-30 17:49:14 +0800

    Select the data from 170ms to 200 ms
    10 data points will be captured in this area.

--- project.m
+++ project.m
@@ -1,7 +1,9 @@
 clear; close all;
-[data1, data2] = loadData();
+[data1, data2, data3, data4, data5, data6, data7, data8] = loadData();
 right = 0;
 total = 0;
+options.MaxIter = 10;
+
 [traindata,testdata] = randomSplit(data1,0.9);
 
 traindata = traindata;
@@ -11,14 +13,14 @@ shape = size(data2);
 numofcol = shape(2);
 numofrow = shape(1);
 
-
-svmmodel = svmtrain(traindata(:,1:numofcol-2),traindata(:,numofcol));
+data = [data1;data2;data6];
+svmmodel = svmtrain(data(:,1:numofcol-2),data(:,numofcol));
 
 predlabel = [];
 for i = 1:1:numofrow
     total = total + 1;%counter
-    predlabel = svmclassify(svmmodel,data2(i,1:numofcol-2));
-    if(data2(i,numofcol)==predlabel) %if the same
+    predlabel = svmclassify(svmmodel,data7(i,1:numofcol-2));
+    if(data7(i,numofcol)==predlabel) %if the same
         right = right + 1;%counter
     end
 end
