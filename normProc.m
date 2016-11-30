2365e97aad5efa2af1bf4328648de57c3aaebc95
Spark-Xl <sparkshenxl@gmail.com>
2016-12-01 03:46:08 +0800

    try to normalize the response from 128 different channels

--- normProc.m
+++ normProc.m
@@ -1,3 +1,24 @@
 function xn = normProc(x)
-    xn = normr(x);   
+    datasize = size(x);
+    numofrow = datasize(1);
+    numofcol = datasize(2);
+    
+% for i = 1:1:numofcol
+%     max_in_data = max(x(:, i));
+%     min_in_data = min(x(:, i));
+%     x(:, i) = (x(:, i) - min_in_data)/(max_in_data - min_in_data); 
+% end
+for i = 1:1:numofcol
+    max_in_data = max(x(i, :));
+    min_in_data = min(x(i, :));
+    x(i, :) = (x(i, :) - min_in_data)/(max_in_data - min_in_data); 
+end
+% for i = 1:1:128
+%     for j = 1:1:60
+%     max_in_data = max(x(10*(j-1) + 1: 10*j, i));
+%     min_in_data = min(x(10*(j-1) + 1: 10*j, i));
+%     x(10*(j-1) + 1: 10*j, i) = (x(10*(j-1) + 1: 10*j, i) - min_in_data)/(max_in_data - min_in_data); 
+%     end
+% end
+    xn = 10 * x;
 end
