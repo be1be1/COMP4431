157bbf5d7d365ea474273f01148244a158482a04
Spark-Xl <sparkshenxl@gmail.com>
2016-12-01 17:55:14 +0800

    try to use mean value for normalization and use cube to increase the distance between data

--- normProc.m
+++ normProc.m
@@ -2,23 +2,17 @@ function xn = normProc(x)
     datasize = size(x);
     numofrow = datasize(1);
     numofcol = datasize(2);
-    
-% for i = 1:1:numofcol
-%     max_in_data = max(x(:, i));
-%     min_in_data = min(x(:, i));
-%     x(:, i) = (x(:, i) - min_in_data)/(max_in_data - min_in_data); 
-% end
 for i = 1:1:numofcol
-    max_in_data = max(x(i, :));
-    min_in_data = min(x(i, :));
-    x(i, :) = (x(i, :) - min_in_data)/(max_in_data - min_in_data); 
+    max_in_data = max(x(:, i));
+    min_in_data = min(x(:, i));
+    mean_in_data = mean(x(:, i));
+    x(:, i)  = (x(:, i) - mean_in_data) / (max_in_data - min_in_data);
 end
-% for i = 1:1:128
-%     for j = 1:1:60
-%     max_in_data = max(x(10*(j-1) + 1: 10*j, i));
-%     min_in_data = min(x(10*(j-1) + 1: 10*j, i));
-%     x(10*(j-1) + 1: 10*j, i) = (x(10*(j-1) + 1: 10*j, i) - min_in_data)/(max_in_data - min_in_data); 
-%     end
+% for i = 1:1:numofrow
+%     tmp = x(i, :);
+%     max_in_data = max(x(i, :));
+%     min_in_data = min(x(i, :));
+%     x(i, :) = (x(i, :) - min_in_data)/(max_in_data - min_in_data); 
 % end
-    xn = 10 * x;
-end
+    xn = x .* x .* x;
+% end
