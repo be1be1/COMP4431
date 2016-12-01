7ddd8d2acffc8b57b1e926a0342a726a6855ad98
Spark-Xl <sparkshenxl@gmail.com>
2016-12-01 21:38:39 +0800

    magic normalization

--- normProc.m
+++ normProc.m
@@ -1,18 +1,10 @@
 function xn = normProc(x)
     datasize = size(x);
     numofrow = datasize(1);
-    numofcol = datasize(2);
-for i = 1:1:numofcol
-    max_in_data = max(x(:, i));
-    min_in_data = min(x(:, i));
-    mean_in_data = mean(x(:, i));
-    x(:, i)  = (x(:, i) - mean_in_data) / (max_in_data - min_in_data);
+for i = 1:1:numofrow/2
+    max_in_data = max(x(i, :));
+    min_in_data = min(x(i, :));
+    x(i, :) = (x(i, :) - min_in_data)/(max_in_data - min_in_data); 
 end
-% for i = 1:1:numofrow
-%     tmp = x(i, :);
-%     max_in_data = max(x(i, :));
-%     min_in_data = min(x(i, :));
-%     x(i, :) = (x(i, :) - min_in_data)/(max_in_data - min_in_data); 
-% end
-    xn = x .* x .* x;
-% end
+    xn = x;
+end
